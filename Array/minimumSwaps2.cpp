d1772502e26287b10e146b82240172d80d6fdded
Kunal Tomar <111980003+CodeKunalTomar@users.noreply.github.com>
2023-08-27 23:41:00 +0530

    Add files via upload

---
+++ Array/minimumSwaps2.cpp
@@ -0,0 +1,116 @@
+/*You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. Find the minimum number of swaps required to sort the array in ascending order.
+
+Sample Input 0
+
+4
+4 3 1 2
+
+Sample Output 0
+
+3
+
+Sample Input 1
+
+5
+2 3 4 1 5
+
+Sample Output 1
+
+3
+
+Sample Input 2
+
+7
+1 3 5 2 4 6 7
+
+Sample Output 2
+
+3
+
+This problem can be solved using graph theory and cycle detection. The idea is to treat the array elements as nodes in a graph, and each element's value represents its correct position. Then, you find cycles in this graph and calculate the number of swaps needed within each cycle. The minimum number of swaps required will be the sum of swaps needed in all cycles.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumSwaps function below.
+int minimumSwaps(vector<int> arr) {
+    int n = arr.size();
+    
+    int swaps = 0;
+    
+    int i = 0;
+    for(int i = 0; i < n; i++)
+    {
+        while(arr[i] != i + 1)
+        {
+            swap(arr[i], arr[arr[i] - 1]);
+            swaps++;
+        }
+    }
+    return swaps;
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
