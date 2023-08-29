0a05c47c67fcf56dc783352c493aedc63f4f823c
Kunal Tomar <111980003+CodeKunalTomar@users.noreply.github.com>
2023-08-29 16:33:00 +0530

    Add files via upload

---
+++ String/alternatingCharacters.cpp
@@ -0,0 +1,111 @@
+You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+Your task is to find the minimum number of required deletions.
+
+Example
+
+s = AABAAB
+
+Remove an A at positions 0 and 3 to make s = ABAB in 2 deletions.
+
+Sample Input
+
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+Sample Output
+
+3
+4
+0
+0
+4
+
+Explanation
+
+The characters marked red are the ones that can be deleted so that the string does not have matching adjacent characters.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'alternatingCharacters' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int alternatingCharacters(string s) {
+    
+    int n = s.size();
+    
+    int j = 1;
+    int deletions = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(j >= n)
+        {
+            break;
+        }
+        
+        if(s[i] == s[j])
+        {
+           deletions++; 
+        }
+        j++;
+    }
+    return deletions;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = alternatingCharacters(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
