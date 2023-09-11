caef00fd8de3ae81c9c3c93fe133d2e291661465
Kunal Tomar <111980003+CodeKunalTomar@users.noreply.github.com>
2023-09-12 01:03:26 +0530

    Update linkedListCycle.cpp

--- Linked List/linkedListCycle.cpp
+++ Linked List/linkedListCycle.cpp
@@ -1,4 +1,4 @@
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
@@ -25,7 +25,7 @@ Example 3:
 Input: head = [1], pos = -1
 Output: false
 Explanation: There is no cycle in the linked list.
-
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
